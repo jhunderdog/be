797c2f1bbd52962c21ba18de60554ba0b98a5fd9
jhunderdog <33382water@gmail.com>
2023-02-18 21:17:51 +0900

    GraphQL

---
+++ 05-04-graphql-api-with-apollo-server-phone/index.js
@@ -0,0 +1,91 @@
+import  { ApolloServer, gql } from 'apollo-server';
+import { checkValidationPhone, getToken, sendTokenToSMS } from './phone.js';
+
+const myTypeDefs = gql`
+  input CreateBoardInput {
+    writer: String
+    title: String
+    contents: String
+ }
+  type BoardReturn {
+    number: Int
+    writer: String
+    title: String
+    contents: String
+  }
+
+  type Query {
+    # fetchBoards: BoardReturn => 객체 1개를 의미
+    fetchBoards: [BoardReturn] # => 배열 안에 객체 1개 이상을 의미
+  }
+
+  type Mutation {
+    createBoard(writer: String, title: String, contents: String): String
+    
+  }
+
+  type Mutation {
+    createBoard2(createBoardInput: CreateBoardInput): String 
+  }
+
+  type Mutation {
+    createTokenOfPhone(myphone: String): String
+  }
+  
+`;
+
+// A map of functions which return data for the schema.
+const myResolvers = {
+  Query: {
+    fetchBoards: () => {
+      const result = [
+        {
+          number: 1,
+          writer: '철수',
+          title: '제목입니다~~',
+          contents: '내용이에요@@@',
+        },
+        {
+          number: 2,
+          writer: '영희',
+          title: '영희 제목입니다~~',
+          contents: '영희 내용이에요@@@',
+        },
+        {
+          number: 3,
+          writer: '훈이',
+          title: '훈이 제목입니다~~',
+          contents: '훈이 내용이에요@@@',
+        },
+      ];
+      return result
+    }
+  },
+  Mutation: {
+    createBoard : (_, args) => {
+      console.log(args)
+      return "등록에 성공하였습니다!!"
+    },
+    createBoard2 : (_, args) => {
+      console.log(args)
+      return "등록에 성공하였습니다!!"
+    },
+    createTokenOfPhone: (_, args) => {
+      const isValid = checkValidationPhone(args.myphone)
+      if(isValid){
+        const mytoken = getToken()
+        sendTokenToSMS(args.myphone, mytoken)
+        return "인증완료!!"
+      }
+    }
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs: myTypeDefs,
+  resolvers: myResolvers,
+});
+
+server.listen(3001).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url} on port ${3001}`);
+});
