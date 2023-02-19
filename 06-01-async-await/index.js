9db87f72233f01cfe464d432f303fe77da56e1a4
jhunderdog <33382water@gmail.com>
2023-02-19 10:52:23 +0900

    async await

---
+++ 06-01-async-await/index.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+//비동기방식
+function fetchPost(){
+    const result = axios.get("https://koreanjson.com/posts/1")
+    console.log(result); //Promise {<pending>}
+}
+
+fetchPost()
+
+//동기방식
+async function fetchPost2(){
+    const result = await axios.get("https://koreanjson.com/posts/1")
+    console.log(result.data); //실제데이터
+}
+
+fetchPost2()
