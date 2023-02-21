143e05005105747222f427d51d9c04ee153084fc
jhunderdog <33382water@gmail.com>
2023-02-21 21:21:53 +0900

    ODM - MongoDB

---
+++ 08-02-docker-compose-with-mongoose/backend/models/board.model.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const BoardSchema = new mongoose.Schema({
+    writer: String,
+    title: String,
+    contents: String
+})
+
+export const Board = mongoose.model("Board", BoardSchema)
+
